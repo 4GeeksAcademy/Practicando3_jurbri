410bca242b1f0535a874e31d43e4b35f13d2568a
JurBri <89058157+JurBri@users.noreply.github.com>
2024-07-14 21:52:33 +0000

    ladin page recetas de comida

---
+++ src/js/component/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => (
+  <footer className="bg-light text-center py-4">
+    <div className="container">
+      <p>&copy; 2024 Recetas de Cocina. Todos los derechos reservados.</p>
+    </div>
+  </footer>
+);
+
+export default Footer;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,20 @@
-import React from "react";
+// Home.js
+import React from 'react';
+import Categories from './categories';
+import Header from './header';
+import Footer from './footer';
+import Navbar from './navbar';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+function Home() {
+  return (
+    <div className="container-fluid">
+		<Navbar/>
+		<Header/>
+		<Categories/>
+		<Footer/>
+      
+    </div>
+  );
+}
 
 export default Home;
